be4c7d1efdcb800e4d9f0080ed7d6a283ba7f252
CTTPamela <pamela.chang@philips.com>
2025-08-11 14:05:19 +0800

    Update monitor.py
    updated

--- monitor.py
+++ monitor.py
@@ -1,29 +1,23 @@
-
-from time import sleep
-import sys
-
-def output():
-  print('\r* ', end='')
-  sys.stdout.flush()
-  sleep(1)
-  print('\r *', end='')
-  sys.stdout.flush()
-  sleep(1)
-
+def pause():
+	for i in range(6)
+	  print('\r* ', end='')
+	  sys.stdout.flush()
+	  sleep(1)
+	  print('\r *', end='')
+	  sys.stdout.flush()
+	  sleep(1)
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      output()
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      output()
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      output()
-    return False
-  return True
+	var error=False
+	if temperature > 102 or temperature < 95:
+		print('Temperature critical!')
+		error=True
+	if pulseRate < 60 or pulseRate > 100:
+		print('Pulse Rate is out of range!')
+		error=True
+	if spo2 < 90:
+		print('Oxygen Saturation out of range!')
+		error=True
+	if error==True:
+		pause()
+		return False
+	return True
